b974edeb9206e4696185c2c591c0204c115f037c
DeveloperTheExplorer <arvin.ansari68@gmail.com>
2024-06-29 11:47:24 -0700

    fix: build error

--- lib/chat/actions.tsx
+++ lib/chat/actions.tsx
@@ -312,7 +312,7 @@ export const AI = createAI<AIState, UIState>({
       const aiState = getAIState()
 
       if (aiState) {
-        const uiState = getUIStateFromAIState(aiState)
+        const uiState = getUIStateFromAIState(aiState as Chat)
         return uiState
       }
     } else {
